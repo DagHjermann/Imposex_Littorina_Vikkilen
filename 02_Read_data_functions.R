20c1817d51a7cd03ff3f2383c96bb8961493284e
Dag_Hjermann <dhj@niva.no>
2019-06-20 16:30:39 +0200

    Improve read_intersex_type1

--- 02_Read_data_functions.R
+++ 02_Read_data_functions.R
@@ -4,9 +4,21 @@ read_intersex_type1 <- function(filename, sheetname, headerline){
   cat("Reading", filename, "\n")
   cat("Sheet", sheetname, "\n")
   
-  # Initial read of all data
-  range <- sprintf("A%i:Y250", headerline)
-  dat_init <- read_excel(filename, sheet = sheetname, range = range)
+  # The next line ('columntypes') was constructed from the output of 
+  # example_of_dataframe %>% map_chr(class) %>% dput()
+  # in this case, specifically:
+  # data_list_onefile[[1]]$data %>% map_chr(class) %>% dput()
+  columntypes <- c(Nr = "numeric", Code = "text", 
+                   SH = "numeric", SB = "numeric", yr = "numeric", VS = "text", 
+                   Pr = "text", Vd = "text", Pe_gl = "numeric", P_P = "numeric", 
+                   Gen_excr = "numeric", Other_excr = "numeric", O = "text", 
+                   Od = "text", Ag = "text", Ig = "text", Cg = "text", 
+                   Vag = "text", in.Od = "text", stage = "numeric", type = "text", 
+                   male = "numeric", Kommentar = "text")
+
+    # Initial read of all data
+  range <- sprintf("A%i:W250", headerline)                            # hard-coded
+  dat_init <- read_excel(filename, sheet = sheetname, range = range, col_types = columntypes)
   colnames(dat_init)[1] <- "Nr"
   # Pe	P	Gen.	other
   # gl	P	excr.	excr.
@@ -16,25 +28,30 @@ read_intersex_type1 <- function(filename, sheetname, headerline){
   colnames(dat_init)[9:12] <- c("Pe_gl", "P_P", "Gen_excr", "Other_excr")
   dat_init
   # Pick "cuts" in data
-  sel <- substr(dat_init$Nr, 1, 4) %in% "----"
+  sel <- substr(dat_init$Code, 1, 4) %in% "----"
   cut <- which(sel)
   cut
   # Read part 1 (males)
-  range <- sprintf("A%i:Y%i", headerline + cut[1] + 1, headerline + cut[2] - 1); range
-  dat_part1 <- read_excel(filename, sheet = sheetname, col_names = FALSE, range = range)
+  range <- sprintf("A%i:W%i", headerline + cut[1] + 1, headerline + cut[2] - 1); range
+  dat_part1 <- read_excel(filename, sheet = sheetname, col_names = FALSE, range = range, col_types = columntypes)
   colnames(dat_part1) <- colnames(dat_init)
   # Read part 2 (females)
-  range <- sprintf("A%i:Y%i", headerline + cut[2] + 1, headerline + cut[3] - 1); range
-  dat_part2 <- read_excel(filename, sheet = sheetname, col_names = FALSE, range = range)
+  range <- sprintf("A%i:W%i", headerline + cut[2] + 1, headerline + cut[3] - 1); range
+  dat_part2 <- read_excel(filename, sheet = sheetname, col_names = FALSE, range = range, col_types = columntypes)
   colnames(dat_part2) <- colnames(dat_init)
   
   # Sex for each part
-  cat("Part 1:", dat_init[cut[1] + 0:1,] %>% pull(kommentar), ";  ")
-  cat("Part 2:", dat_init[cut[2] + 0:1,] %>% pull(kommentar), "\n")
+  cat("Part 1:", dat_init[cut[1] + 0:1,] %>% pull(Kommentar), ";  ")
+  cat("Part 2:", dat_init[cut[2] + 0:1,] %>% pull(Kommentar), "\n")
 
+  # Extract station name and year from the sheet name
+  sheetname_split <- strsplit(sheetname, split = "_")[[1]]
+  station <- sheetname_split[1]
+  year <- as.numeric(sheetname_split[2]) + 2000
+  
   data <- bind_rows(
-    data.frame(Sheet = sheetname, Sex = "M", dat_part1, stringsAsFactors = FALSE),   # NOTE: we assume that part 1 is always the males and part 2 is the females
-    data.frame(Sheet = sheetname, Sex = "F", dat_part2, stringsAsFactors = FALSE)
+    data.frame(Sheet = sheetname, Station = station, Year = year, Sex = "M", dat_part1, stringsAsFactors = FALSE),   # NOTE: we assume that part 1 is always the males and part 2 is the females
+    data.frame(Sheet = sheetname, Station = station, Year = year, Sex = "F", dat_part2, stringsAsFactors = FALSE)
   ) 
   
   list(data = as_tibble(data), colnames = colnames_original)
